fda35bd2c9191efaa986d7c4fd05613e8ef69fc7
lenaabdu <54982546+lenaabdu@users.noreply.github.com>
2020-07-18 00:55:25 +0100

    Update tester.spec.js

--- tester.spec.js
+++ tester.spec.js
@@ -4,6 +4,10 @@ import { checkState } from "./tester"
 describe("state", () => {
     it("should be alive", () => {
         const result = checkState("0")
-        expect(result).toBe("alive");
+        expect(result).toBe(true);
+    })
+    it("should be adead", () => {
+        const result = checkState("1")
+        expect(result).toBe(false);
     })
 })
