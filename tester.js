66d9f5e65d154aeeb3d44f198807554991ab7375
lenaabdu <54982546+lenaabdu@users.noreply.github.com>
2020-07-19 19:33:22 +0100

    Update tester.js
    7 test  7 passed

--- tester.js
+++ tester.js
@@ -7,19 +7,16 @@ class Cell {
 
     getNextState(numOfNeighbours) {
 
-        if (this.state === cellState.ALIVE) {   //when the initial state of the cell is ALIVE
-            if (numOfNeighbours < 2) {
-                return cellState.DEAD;
-            } else if (numOfNeighbours > 3) {
-                return cellState.DEAD;
-            } else if (numOfNeighbours === 2 || numOfNeighbours === 3) {
+        if (this.state === cellState.ALIVE) { //when the initial state of the cell is ALIVE
+            if (numOfNeighbours === 2 || numOfNeighbours === 3) {
                 return cellState.ALIVE;
             }
-        } else {  //when the initial state of the cell is DEAD
-
+         } else if (numOfNeighbours === 3) {
+            return cellState.ALIVE;
         }
+        return cellState.DEAD; //when the initial state of the cell is DEAD
+    }
+}
 
-    };
-};
 export { Cell };
 export { cellState };
