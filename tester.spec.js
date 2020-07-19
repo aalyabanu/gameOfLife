0f621a694613a15864f2bc72af3d0108c0fcfc2f
lenaabdu <54982546+lenaabdu@users.noreply.github.com>
2020-07-19 11:02:40 +0100

    Update tester.spec.js

--- tester.spec.js
+++ tester.spec.js
@@ -1,20 +1,35 @@
-import { cellState } from "./tester"
+import { cellState } from "./tester";
+import { Cell } from "./tester";
 
 describe("cell state", () => {
     it("should be ALIVE", () => {
         const result = cellState.ALIVE
         expect(result).toEqual(1);
     })
-    it("should be dead", () => {
+    it("should be DEAD", () => {
         const result = cellState.DEAD
         expect(result).toEqual(0);
     })
 })
 
 //assigning a cell with initial state
-describe('Cell', () => {
+describe("Cell ", () => {
     it("should be assigned with cellState", () => {
-        const cell = new Cell(cellState.ALIVE) //creating new instance of Cell object and assigning it with alive state
-        expect(cell.state).toEqual(cellState.AlIVE); //expecting to be initialized to ALIVE
+        const alivecell = new Cell(cellState.ALIVE) //creating new instance of Cell object and assigning it with alive state
+        expect(alivecell.state).toEqual(cellState.ALIVE);
+        const deadcell = new Cell(cellState.DEAD)
+        expect(deadcell.state).toEqual(cellState.DEAD);
+
     });
+
+    // Frist condition of the cell (Any live cell with fewer than two live neighbours dies as if by under population.)
+    it('Should die if it has fewer than 2 live nieghbors', () => {
+        const cell = new Cell(CellState.ALIVE);
+        const StateWith0Neighbors = cell.getNextState(0);
+        expect(StateWith0Neighbors).toEqual(CellState.DEAD); //expecting to be initialized to DEAD
+
+        const StateWith1Neighbor = cell.getNextState(1);
+        expect(StateWith1Neighbor).toEqual(CellState.DEAD);
+    });
+
 });
