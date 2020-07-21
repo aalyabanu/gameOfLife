6d2338d09a52c126e42d54b298724078484827a4
Aalya <aalya.banu@gmail.com>
2020-07-21 17:12:38 +0100

    unnecessary files removed

--- dummyfile.js
+++
@@ -1,9 +0,0 @@
-arr1 = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
-rows = arr1.length;
-console.log(rows);
-cols = arr1[0].length;
-console.log(cols);
-arr2 = arr1.map(row => row.map(x => x * x));
-console.log(arr2);
-
-console.log(arr1(1, 1));
